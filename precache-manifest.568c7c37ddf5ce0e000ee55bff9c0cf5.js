3c1383d3cf48083031045ec2d23a91e078b1f687
pratikpc <pratikpc@users.noreply.github.com>
2020-09-01 22:10:55 +0000

    deploy: 97350bd4975ff8ece6920dbe4b85021a59d109cd

---
+++ precache-manifest.568c7c37ddf5ce0e000ee55bff9c0cf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d28ccda02312eff8a5684e3b1491c4",
+    "url": "/six-degree-f1/index.html"
+  },
+  {
+    "revision": "60d83efa1893e18f5a79",
+    "url": "/six-degree-f1/static/css/main.fb8e7c72.chunk.css"
+  },
+  {
+    "revision": "e226a44899139eb202aa",
+    "url": "/six-degree-f1/static/js/2.71f4d5af.chunk.js"
+  },
+  {
+    "revision": "9ade2227599194d346aa406001a5b5ef",
+    "url": "/six-degree-f1/static/js/2.71f4d5af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d83efa1893e18f5a79",
+    "url": "/six-degree-f1/static/js/main.085ae167.chunk.js"
+  },
+  {
+    "revision": "ed4ce458dcc8913d4bf6",
+    "url": "/six-degree-f1/static/js/runtime-main.7261cee9.js"
+  }
+]);
